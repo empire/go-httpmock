3f3b7526d4ac3c3bd8b771822a681fe3a372e466
Tomas Aparicio <tomas@aparicio.me>
2016-11-10 23:23:30 +0000

    fix: dependencies

--- _examples/gentleman/gentleman.go
+++ _examples/gentleman/gentleman.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/h2non/gentleman.v0"
-	"gopkg.in/h2non/gentleman.v0/context"
-	"gopkg.in/h2non/gock.v1"
+
+	"gopkg.in/h2non/gentleman.v1/context"
 )
 
 // Usege example with gentleman HTTP client toolkit.
