d6983caced3642327f0c15b0a51f85b1ab838996
wfscot <6472550+wfscot@users.noreply.github.com>
2021-06-02 04:55:25 -0600

    Add context expiration cancellation support (#86)
    * Add support for expired and cancelled request contexts
    
    This should make gock behave the same as the standard HTTP client when
    it comes to request context behavior.
    
    * Update go.mod based on context support
    
    http.NewRequestWithContext was introduced in go v1.13 so that is now the
    minimum required version. Also remove unused modules via go mod tidy.
    
    * Fix gofmt errors in original commit

--- responder_test.go
+++ responder_test.go
@@ -1,10 +1,12 @@
 package gock
 
 import (
+	"context"
 	"errors"
 	"io/ioutil"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/nbio/st"
 )
@@ -61,5 +63,58 @@ func TestResponderError(t *testing.T) {
 
 	res, err := Responder(req, mres, nil)
 	st.Expect(t, err.Error(), "error")
+	st.Expect(t, res == nil, true)
+}
+
+func TestResponderCancelledContext(t *testing.T) {
+	defer after()
+	mres := New("http://foo.com").Get("").Reply(200).Delay(20 * time.Millisecond).BodyString("foo")
+
+	// create a context and schedule a call to cancel in 10ms
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		time.Sleep(10 * time.Millisecond)
+		cancel()
+	}()
+
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://foo.com", nil)
+
+	res, err := Responder(req, mres, nil)
+
+	// verify that we got a context cancellation error and nil response
+	st.Expect(t, err, context.Canceled)
+	st.Expect(t, res == nil, true)
+}
+
+func TestResponderExpiredContext(t *testing.T) {
+	defer after()
+	mres := New("http://foo.com").Get("").Reply(200).Delay(20 * time.Millisecond).BodyString("foo")
+
+	// create a context that is set to expire in 10ms
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+	defer cancel()
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://foo.com", nil)
+
+	res, err := Responder(req, mres, nil)
+
+	// verify that we got a context cancellation error and nil response
+	st.Expect(t, err, context.DeadlineExceeded)
+	st.Expect(t, res == nil, true)
+}
+
+func TestResponderPreExpiredContext(t *testing.T) {
+	defer after()
+	mres := New("http://foo.com").Get("").Reply(200).BodyString("foo")
+
+	// create a context and wait to ensure it is expired
+	ctx, cancel := context.WithTimeout(context.Background(), 500*time.Microsecond)
+	defer cancel()
+	time.Sleep(1 * time.Millisecond)
+	req, _ := http.NewRequestWithContext(ctx, http.MethodGet, "http://foo.com", nil)
+
+	res, err := Responder(req, mres, nil)
+
+	// verify that we got a context cancellation error and nil response
+	st.Expect(t, err, context.DeadlineExceeded)
 	st.Expect(t, res == nil, true)
 }
